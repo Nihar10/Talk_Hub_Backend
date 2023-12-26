35fc3470c3d8b47ce9a244e13b2245bf04be0fd0
Surveer Singh <hs309123@gmail.com>
2023-12-26 23:14:51 +0530

    changes

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://beamish-lebkuchen-cf4551.netlify.app/",
     credentials: true,
   },
 });
